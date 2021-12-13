41dcf11083006fc40010d95e159f3a0fea9c1ef3
mcorsano <mariannacorsano98@gmail.com>
2021-12-13 01:41:44 +0100

    phrase queries improvements

--- PhraseQuery.py
+++ PhraseQuery.py
@@ -11,6 +11,8 @@ class PhraseQuery :
     def find(self, query, documents, terms) :
         query_words = query.split()
         index = 0
+        intermediate_dict = {}
+
         while (index < len(query_words)-1) :
             word1 = lemmatizer.lemmatize(query_words[index].lower())
             word2 = lemmatizer.lemmatize(query_words[index+1].lower())
@@ -20,17 +22,6 @@ class PhraseQuery :
             posting_l1 = term1.get_posting_list()
             posting_l2 = term2.get_posting_list()
 
-            #intersection_posting_l = InvertedIndex().intersection(term1, term2)
-           
-            '''
-            for posting in intersection_posting_l :
-                doc = documents.get(posting)
-                positions_1 = doc.get_term_positions(term1)
-                positions_2 = doc.get_term_positions(term2)
-
-                print(positions_1)
-                print(positions_2)
-            '''
             position_of_first = []
             i = 0
             j = 0
@@ -43,8 +34,12 @@ class PhraseQuery :
                     l = 0
                     while (k < len(positions_1) and l < len(positions_2)) :
                         if (positions_1[k] == (positions_2[l] - 1)) :
-                            print('trovato')
-                            position_of_first.append(positions_1[k])
+                            if (doc not in intermediate_dict.keys()) :
+                                intermediate_dict[doc] = [positions_1[k]]
+                            else :
+                                intermediate_dict[doc].append(positions_1[k])
+                            #print('trovato')
+                            #position_of_first.append(positions_1[k])
                             k += 1
                             l += 1
                         elif (positions_1[k] < (positions_2[l] - 1)) :
@@ -57,12 +52,15 @@ class PhraseQuery :
                     i += 1
                 else :
                     j += 1
-            print(position_of_first)
+            #print(position_of_first)
+            #print(intermediate_dict)
 
 
                 
             index += 1
-    
-        return 'fine della query'
+        
+        answer_to_query = {key: value for key, value in intermediate_dict.items() if (len(value) == (len(query_words) -1))}
+        
+        return answer_to_query
         
 
